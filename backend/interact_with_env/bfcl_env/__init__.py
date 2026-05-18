537aa1d74c7b1155e1ac354b135fea4ca27d5b9b
Hải Yến <nhyen.workspace@gmail.com>
2026-05-18 10:55:22 +0700

    Delete backend/interact_with_env/bfcl_env directory

--- backend/interact_with_env/bfcl_env/__init__.py
+++
@@ -1,6 +0,0 @@
-from .env import BfclEnv
-# from .env_revise import BfclEnv
-
-print("这是env.py")
-
-__all__ = ["BfclEnv"]
--- backend/interact_with_env/bfcl_env/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bfcl_tools import mean, INVOLVED_CLASS_TO_FUNC_DOC_PATH
-
-__all__ = [ "mean", 
-           "INVOLVED_CLASS_TO_FUNC_DOC_PATH"] 
